99f0ab6bcb9a8bf013203e764b273c883ed4b50a
sunS20 <ghddptjs@cau.ac.kr>
2023-11-29 14:28:33 +0900

    테스트

---
+++ lib/TodoScreen.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:flutter/src/painting/image_provider.dart';
+import 'dart:io';
+import 'dart:core';
+
+class TodoItem {
+  String title; // 할 일 항목의 제목
+  bool isCompleted;// 할 일 항목의 완료 상태
+
+  TodoItem({required this.title, this.isCompleted = false});
+}
+
+class TodoScreen extends StatefulWidget {
+  static const routeName = '/todo';
+  @override
+  _TodoScreenState createState() => _TodoScreenState();
+}
+
+class _TodoScreenState extends State<TodoScreen> {
+  final List<TodoItem> todoList = [];
+  TextEditingController textEditingController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("My Checklist"),
+        centerTitle: true,
+        backgroundColor:Colors.deepPurple,
+        foregroundColor: Colors.white,
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    focusNode: fnode,
+                    controller: textEditingController,
+                    cursorColor: Colors.grey,
+                    decoration: InputDecoration(labelText: "Add a new task",labelStyle:TextStyle(color:fnode.hasFocus? Colors.deepPurple : Colors.grey),focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.deepPurple)))  ,
+                  ),
+                ),
+                IconButton(
+                  icon: Icon(Icons.add),
+                  onPressed: () {
+                    addTodoItem(textEditingController.text);
+                    textEditingController.clear();
+                  },
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: todoList.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  leading: Checkbox(
+                    activeColor: Colors.white,
+                    checkColor: Colors.deepPurple,
+                    value: todoList[index].isCompleted,
+                    onChanged: (value) {
+                      toggleTodoItem(index);
+                    },
+                  ),
+                  title: Text(todoList[index].title),
+                  trailing: IconButton(
+                    icon: Icon(Icons.delete),
+                    onPressed: () {
+                      deleteTodoItem(index);
+                    },
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void addTodoItem(String title) {
+    setState(() {
+      todoList.add(TodoItem(title: title, isCompleted: false));
+    });
+  }
+
+  void deleteTodoItem(int index) {
+    setState(() {
+      todoList.removeAt(index);
+    });
+  }
+
+  void toggleTodoItem(int index) {
+    setState(() {
+      todoList[index].isCompleted = !todoList[index].isCompleted;
+    });
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/src/material/page.dart';
 import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
 import 'dart:core';
 import 'MyProfile.dart';
+import 'TodoScreen.dart';
 import 'package:swap_life/show_login.dart';
 
 FocusNode fnode=new FocusNode();
@@ -10,6 +11,7 @@ FocusNode fnode=new FocusNode();
 void main() {
   KakaoSdk.init(nativeAppKey: 'e7a7bba0f8d93f336d1343d3f47222ae',
   );
+
   runApp(MyApp());
 }
 
@@ -24,7 +26,7 @@ class MyApp extends StatelessWidget {
         '/': (context) => ShowLogin(),
         '/myHome': (context) => MyHome(),
         '/myProfile' : (context) => MyProfile(),
-        TodoScreen.routeName: (context) => TodoScreen(),
+        '/todo': (context) => TodoScreen(),
       },
       debugShowCheckedModeBanner: false,
     );//상태값 변하므로 Stateful위젯 사용
@@ -127,102 +129,3 @@ class _MyHomeState extends State<MyHome> with SingleTickerProviderStateMixin {
     );
   }
 }
-
-class TodoItem {
-  String title; // 할 일 항목의 제목
-  bool isCompleted;// 할 일 항목의 완료 상태
-
-  TodoItem({required this.title, this.isCompleted = false});
-}
-
-class TodoScreen extends StatefulWidget {
-  static const routeName = '/todo';
-  @override
-  _TodoScreenState createState() => _TodoScreenState();
-}
-
-class _TodoScreenState extends State<TodoScreen> {
-  final List<TodoItem> todoList = [];
-  TextEditingController textEditingController = TextEditingController();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("My Checklist"),
-        centerTitle: true,
-        backgroundColor:Colors.deepPurple,
-        foregroundColor: Colors.white,
-      ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Row(
-              children: [
-                Expanded(
-                  child: TextField(
-                    focusNode: fnode,
-                    controller: textEditingController,
-                    cursorColor: Colors.grey,
-                    decoration: InputDecoration(labelText: "Add a new task",labelStyle:TextStyle(color:fnode.hasFocus? Colors.deepPurple : Colors.grey),focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.deepPurple)))  ,
-                  ),
-                ),
-                IconButton(
-                  icon: Icon(Icons.add),
-                  onPressed: () {
-                    addTodoItem(textEditingController.text);
-                    textEditingController.clear();
-                  },
-                ),
-              ],
-            ),
-          ),
-          Expanded(
-            child: ListView.builder(
-              itemCount: todoList.length,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  leading: Checkbox(
-                    activeColor: Colors.white,
-                    checkColor: Colors.deepPurple,
-                    value: todoList[index].isCompleted,
-                    onChanged: (value) {
-                      toggleTodoItem(index);
-                    },
-                  ),
-                  title: Text(todoList[index].title),
-                  trailing: IconButton(
-                    icon: Icon(Icons.delete),
-                    onPressed: () {
-                      deleteTodoItem(index);
-                    },
-                  ),
-                );
-              },
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  void addTodoItem(String title) {
-    setState(() {
-      todoList.add(TodoItem(title: title, isCompleted: false));
-    });
-  }
-
-  void deleteTodoItem(int index) {
-    setState(() {
-      todoList.removeAt(index);
-    });
-  }
-
-  void toggleTodoItem(int index) {
-    setState(() {
-      todoList[index].isCompleted = !todoList[index].isCompleted;
-    });
-  }
-}
-
